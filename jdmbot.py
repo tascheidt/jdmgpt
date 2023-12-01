43545f0f73eb2bf2fc46c71f9c87a1122d684b56
tascheidt <88148089+tascheidt@users.noreply.github.com>
2023-12-01 10:28:45 -0500

    v1.1 - working bot with persistent store

--- jdmbot.py
+++ jdmbot.py
@@ -5,7 +5,7 @@ import openai
 import os
 import chromadb
 from llama_index.vector_stores import ChromaVectorStore
-from llama_index import SimpleDirectoryReader
+from llama_index import SimpleDirectoryReader, StorageContext
 
 st.set_page_config(page_title="Chat with open access JDM research", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -18,12 +18,25 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
     ]
 
 @st.cache_resource(show_spinner=False)
+
 def load_data():
     with st.spinner(text="Loading and indexing the JDM research docs – hang tight! This should take 1-2 minutes."):
+        # build persistent chroma client
+        chroma_client = chromadb.PersistentClient(path="/Users/tscheidt/Documents/code/jdmgpt/chroma_db")
+        chroma_collection = chroma_client.get_or_create_collection("gptbot") # create a new collection if doesn't exist
+        print(chroma_collection.name)
+        print(chroma_collection.count())
+
         reader = SimpleDirectoryReader(input_dir="./sample_articles", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        #print(docs[0])
+
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert medical researcher autoimmune conditions with a specialization in juvenile dermatomyositis (JDM) and your job is to answer questions from medical practitioners and patients to help understand the causes and potential treatments of JDM. Assume that JDM means juvenile dermatomyositis and all questions are related to the JDM Research Library. Keep your answers technical and based on facts – do not hallucinate features."))
+        
+        index = VectorStoreIndex.from_documents(docs, storage_context=storage_context, service_context=service_context)
+        #print(index[0:10])
         return index
 
 index = load_data()
